f7c736a9b7209cd262c11142fd4ab9c2b8c93862
kim-shun <kimura1417080@gmail.com>
2021-06-16 08:32:32 +0900

    タグを表示させる

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -3,7 +3,21 @@ if (location.pathname.match("tweets/new")){
     const inputElement = document.getElementById("tweets_tag_name");
     inputElement.addEventListener("keyup", () => {
       const keyword = document.getElementById("tweets_tag_name").value;
-      console.log(keyword);
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `search/?keyword=${keyword}`, true)
+      XHR.responseType = "json"
+      XHR.send();
+      XHR.onload = () => {
+        const tagName = XHR.response.keyword;
+        const searchResult = document.getElementById("search-result");
+        tagName.forEach((tag) => {
+          const childElement = document.createElement("div");
+          childElement.setAttribute("class", "child");
+          childElement.setAttribute("id", tag.id);
+          childElement.innerHTML = tag.name;
+          searchResult.appendChild(childElement);
+        });
+      };
     });
   });
 };
